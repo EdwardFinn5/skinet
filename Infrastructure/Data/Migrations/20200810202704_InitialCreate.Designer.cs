9ce321dc2ac82cbee7a39d247e0ddf4c77ac7dad
Ed Finn <ed@iowacollegefoundation.org>
2020-08-10 18:49:44 -0500

    End of section 3

--- Infrastructure/Data/Migrations/20200810202704_InitialCreate.Designer.cs
+++ Infrastructure/Data/Migrations/20200810202704_InitialCreate.Designer.cs
@@ -8,7 +8,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Infrastructure.Data.Migrations
 {
     [DbContext(typeof(StoreContext))]
-    [Migration("20200809144749_InitialCreate")]
+    [Migration("20200810202704_InitialCreate")]
     partial class InitialCreate
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
